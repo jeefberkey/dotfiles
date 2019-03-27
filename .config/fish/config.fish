707c129410500ebfc36a0d1a1f7bcae32e8b4d80
Nick Miller <nmiller@attilasec.com>
2019-03-27 11:41:06 -0400

    More sway work

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,16 +4,17 @@
 cat ~/.cache/wal/sequences
 
 # rbenv
-status --is-interactive; and source (rbenv init -|psub)
+#status --is-interactive; and source (rbenv init -|psub)
 
 # pyenv
-status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
-systemctl --user set-environment GDK_BACKEND=wayland
-systemctl --user set-environment CLUTTER_BACKEND=wayland
-systemctl --user set-environment QT_QPA_PLATFORM=wayland-egl
-systemctl --user set-environment _JAVA_AWT_WM_NONREPARENTING=1
-systemctl --user set-environment XDG_CURRENT_DESKTOP=Unity
+#systemctl --user set-environment GDK_BACKEND=wayland
+#systemctl --user set-environment CLUTTER_BACKEND=wayland
+#systemctl --user set-environment QT_QPA_PLATFORM=wayland-egl
+#systemctl --user set-environment _JAVA_AWT_WM_NONREPARENTING=1
+#systemctl --user set-environment XDG_CURRENT_DESKTOP=Unity
 
 gpg-connect-agent /bye
 set -x SSH_AUTH_SOCK 'gpgconf --list-dirs agent-ssh-socket'
--- .config/fish/functions/atom.fish
+++ .config/fish/functions/atom.fish
@@ -1,3 +1,4 @@
-function atom --description 'Launch atom-beta with some flags'
-  command atom-beta $argv --enable-gpu-rasterization
-end
+#function atom --description 'Launch atom-beta with some flags'
+#  #command atom-beta $argv --enable-gpu-rasterization
+#  command atom $argv --enable-gpu-rasterization
+#end
