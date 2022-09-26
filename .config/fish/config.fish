6728104d1f5ed1141317ce617cc567db9f62a9d9
Nick Miller <nick.miller54@gmail.com>
2022-09-25 20:48:04 -0400

    sep 2022

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,29 +8,9 @@ end
 # Terminal colors
 #cat ~/.cache/wal/sequences
 
-# pyenv
-# status --is-interactive; and pyenv init - | source
-# status --is-interactive; and pyenv virtualenv-init - | source
-
-#set -Ux _JAVA_AWT_WM_NONREPARENTING 1
-#set -Ux CLUTTER_BACKEND wayland
-#set -Ux ECORE_EVAS_ENGINE wayland_egl
-#set -Ux ELM_ENGINE wayland_egl
-#set -Ux MOZ_ENABLE_WAYLAND 1
-## packer/vbox has issues with this
-##set -Ux QT_QPA_PLATFORM wayland-egl
-#set -Ux XDG_CURRENT_DESKTOP sway
-## electron has problems with this
-##set -Ux GDK_BACKEND wayland
-
-# gpg-connect-agent /bye
-#set -x SSH_AUTH_SOCK 'gpgconf --list-dirs agent-ssh-socket'
-#set -gx SSH_AUTH_SOCK (gnome-keyring-daemon --start | grep "^SSH_AUTH_SOCK" | awk -F "=" '{print $2}')
-#if test -n "$DESKTOP_SESSION"
-#  set (gnome-keyring-daemon --start | string split "=")
-#end
-
-eval (ssh-agent -c)
+# if this doesn't work
+# https://major.io/2022/08/05/use-gnome-keyring-with-sway/
+set -gx SSH_AUTH_SOCK /run/user/(id -u)/keyring/ssh
 
 # set theme using starship https://starship.rs
 eval (starship init fish)
