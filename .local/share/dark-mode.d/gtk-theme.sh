094280bd212ee593b16ae67d84bc56ac3c0d1585
Nick Miller <nick.miller@octoenergy.com>
2023-10-12 11:02:09 -0400

    fix darkman theme switching

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# Note: The names for the Arc theme variations are terrible.
-# "Darker" is actually LESS DARK than "Dark".
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita:dark
--- .local/share/dark-mode.d/xfce-theme.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec xfconf-query -c xsettings -p /Net/ThemeName -s 'Adwaita-dark'
